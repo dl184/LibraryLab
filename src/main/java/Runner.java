fe6ae9b2d7ffcbd1bdc06cb2badc430cbb03a3a9
Derek Leach <derekleach84@gmail.com>
2018-07-11 15:17:30 +0100

    finished seeds

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,10 +1,26 @@
+import db.DBHelper;
 import models.Book;
+import models.Borrower;
+import models.Library;
 
 public class Runner {
 
     public static void main(String[] args) {
 
+        Library library = new Library("Glasgow Library");
+        DBHelper.save(library);
 
-        Book book1 = new Book("Kidnapped", "Richard PK", "")
+        Book book1 = new Book("Trouble in Paradise", "Derek Leach", "Romantic tragedy", library);
+        DBHelper.save(book1);
+        Book book2 = new Book("All Loved Up", "Redek Cheal", "Romance", library);
+        DBHelper.save(book2);
+
+        Borrower borrower1 = new Borrower("Richard", library);
+        DBHelper.save(borrower1);
+        Borrower borrower2 = new Borrower("Derek", library);
+        DBHelper.save(borrower2);
+
+
+
     }
 }
